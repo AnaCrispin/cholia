b025297154a878ba137769cb70a2c13c6fcab282
AnaCrispin <yeyu050689@gmail.com>
2025-08-10 14:39:07 -0400

    fix: index.html meta-redirect + inputs Vite

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import { resolve } from 'path';
 export default defineConfig({
     server: {
         port: 5173,
-        open: '/bruja/inicio.html'
+        open: '/bruja/index.html'
     },
     build: {
         rollupOptions: {
             input: {
                 index: resolve(__dirname, 'index.html'),
-                inicio: resolve(__dirname, 'bruja/inicio.html'),
+                inicio: resolve(__dirname, 'bruja/index.html'),
                 juego: resolve(__dirname, 'juego.html')
             }
         }
