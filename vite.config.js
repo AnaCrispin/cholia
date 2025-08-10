09820aef7795306a4f02aa214572a32da2ebdd51
AnaCrispin <yeyu050689@gmail.com>
2025-08-10 14:45:04 -0400

    Add index.html redirect + Vite inps

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-    server: {
-        port: 5173,
-        open: '/bruja/index.html'
-    },
+    base: '/',                                   // importante en producción
+    server: { port: 5173, open: '/bruja/index.html' },
     build: {
         rollupOptions: {
             input: {
-                index: resolve(__dirname, 'index.html'),
-                inicio: resolve(__dirname, 'bruja/index.html'),
-                juego: resolve(__dirname, 'juego.html')
+                index: resolve(__dirname, 'bruja/index.html'), // genera dist/index.html
+                juego: resolve(__dirname, 'juego.html')        // genera dist/juego.html
             }
         }
     }
-});
+})
